f430e4f9f5db86dd3f48239f8a9d102ceb902031
MasterZeus97 <thibault.seem@heig-vd.ch>
2022-10-04 13:21:22 +0200

    Ajout de 2 tests pour les données de la voiture

--- src/test/java/dataCarTest.java
+++ src/test/java/dataCarTest.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class dataCarTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+/*
+class CalculatorTests {
+
+    @Test
+    @DisplayName("1 + 1 = 2")
+    void addsTwoNumbers() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+    }
+
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "0,    1,   1",
+            "1,    2,   3",
+            "49,  51, 100",
+            "1,  100, 101"
+    })
+    void add(int first, int second, int expectedResult) {
+        Calculator calculator = new Calculator();
+        assertEquals(expectedResult, calculator.add(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+    }
 }
+*/
+
+class dataCarTest {
+    dataCar _dataCar;
+
+    @org.junit.jupiter.api.BeforeEach
+    void beforeEach() throws IOException {
+
+        _dataCar = new dataCar("src/main/resources/dataCar.json");
+    }
+
+    @org.junit.jupiter.api.Test
+    void testPlate(){
+        //JSONObject expectedData = new JSONObject("../../main/resources/dataCar.json");
+
+        String expectedPlate = "GE 123201";
+
+        assertEquals(expectedPlate, _dataCar.getPlate());
+    }
+
+    @org.junit.jupiter.api.Test
+    void testId(){
+        int expectedInteger = 939948275;
+
+        assertEquals(expectedInteger, _dataCar.getId());
+    }
+
+}
+
